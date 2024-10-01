9e89d4547ed907664ed752fada7b80e9cff2be4e
Slevinnnn7 <slevin068@gmail.com>
2024-10-01 11:08:37 +0700

    belajar-express part2
    lanjutan

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require("express") // impor modul express
 const app = express() // inisialisasi express
+const expressLayout = require('express-ejs-layouts0');
 const port = 3000 // port
+
+// Middleware
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+// gunakan ejs
+app.set("view engine", "ejs");
 
 // route /
 app.get("/", (req, res) => {
-    //res.send("Hello");
-    res.sendFile(__dirname + "/home.html");
+    const news = [
+        {id: 1, title: "Berita 1", content: "Content berita 1"},
+        {id: 2, title: "Berita 2", content: "Content berita 2"},
+        {id: 3, title: "Berita 3", content: "Content berita 3"},
+    ];
+    //res.send("Hello World");
+    //res.sendFile(__dirname + "/home.html");
+    res.render("index", {title: "Halaman Home", news});
 });
 
 // route /about
 app.get("/about", (req, res) => {
     //res.send("About Us");
     res.sendFile(__dirname + "/about.html");
+});
+
+// Data program studi
+const prodi = [
+    { nama: 'Sistem Informasi', singkatan: 'SI', fakultas: 'Fakultas Ilmu Komputer dan Rekayasa' },
+    { nama: 'Informatika', singkatan: 'IF', fakultas: 'Fakultas Ilmu Komputer dan Rekayasa' },
+    { nama: 'Manajemen Informatika', singkatan: 'MI', fakultas: 'Fakultas Ilmu Komputer dan Rekayasa' },
+    { nama: 'Teknik Elektro', singkatan: 'TE', fakultas: 'Fakultas Ilmu Komputer dan Rekayasa' },
+    { nama: 'Akuntansi', singkatan: 'AK', fakultas: 'Fakultas Ekonomi dan Bisnis' },
+    { nama: 'Manajemen', singkatan: 'MJ', fakultas: 'Fakultas Ekonomi dan Bisnis' }
+];
+
+// Route /prodi
+app.get('/prodi', (req, res) => {
+    res.render('prodi', { prodi });
 });
 
 // route /contact
