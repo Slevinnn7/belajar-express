5c845a0bf13f549d540631d6e05cf05473e7f6c2
Slevinnnn7 <slevin068@gmail.com>
2024-11-12 10:33:43 +0700

    12-11-2024

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,24 +1,23 @@
-// models/db.js
-// mengimpor modul mongoose untuk mengelola koneksi dengan  MongoDB
+// model db js
+// mengimpor modul mongoose untuk mengelola koneksi dengan mongodb
 const mongoose = require("mongoose");
 
-// fungsi  asinkron untuk menghubungkan ke database MongoDB
+// fungsi asinkron untuk menghubungkan ke database mongodb
 const connectDB = async () => {
-    try{
-        // menghubungkan ke MongoDB menggunakan url koneksi
+    try {
+        //menghubungkan ke mongodb menggunakan url koneksi
         await mongoose.connect(
-            "mongodb+srv://vinn:BelajarMongo2024@cluster0.7hcbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            // "mongodb+srv://ericcandra:eric123@cluster0.hadmb.mongodb.net/mdpdb?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb://localhost:27017/mdpdb"
         );
-        // jika koneksi  berhasil, log pesan ke konsol
-        console.log("MongoDB Connected...");
+        // jika koneksi berhasil log pesan ke konsol
+        console.log("MongoDb Connected");
     } catch (error) {
-        // jika terjadi kesalahan saat menghubungkan, log pesan kesalahan ke ponsel
-        console.error("MongoDB connection server:", error);
-        // keluar dari proses dengan kode status 1 untuk menandakan ada kesalahan 
+        // jika terjadi kesalahan saat menghubungkan, log pesan kesalahan ke konsol
+        console.error("MongoDB connection error:", error);
+        // keluar dari proses dengan kode status 1 untuk menandakan ada kesalahan
         process.exit(1);
     }
 };
 
-
-// mengeksport fungsi connectDB agar dapat digunakan di file lain
 module.exports = connectDB;
