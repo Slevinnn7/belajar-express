475d61813abca70bbad8eec1cb4b3c40fb879075
Slevinnnn7 <slevin068@gmail.com>
2024-11-12 11:06:58 +0700

    semoga

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,23 +1,16 @@
-// model db js
-// mengimpor modul mongoose untuk mengelola koneksi dengan mongodb
-const mongoose = require("mongoose");
 
-// fungsi asinkron untuk menghubungkan ke database mongodb
-const connectDB = async () => {
+const mongoose = require ("mongoose");
+const connectDB = async() =>{
     try {
-        //menghubungkan ke mongodb menggunakan url koneksi
         await mongoose.connect(
-            // "mongodb+srv://ericcandra:eric123@cluster0.hadmb.mongodb.net/mdpdb?retryWrites=true&w=majority&appName=Cluster0"
-            "mongodb://localhost:27017/mdpdb"
+            // "mongodb+srv://<db_username>:<db_password>@cluster0.7hcbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb://localhost:27017/tugasmdp"
         );
-        // jika koneksi berhasil log pesan ke konsol
-        console.log("MongoDb Connected");
-    } catch (error) {
-        // jika terjadi kesalahan saat menghubungkan, log pesan kesalahan ke konsol
-        console.error("MongoDB connection error:", error);
-        // keluar dari proses dengan kode status 1 untuk menandakan ada kesalahan
-        process.exit(1);
+    console.log("Mongodb connected");
     }
+    catch (error){
+        console.error("mongodb connection error", error);
+        process.exit(1);
+    };
 };
-
 module.exports = connectDB;
